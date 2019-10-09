8da90a90916e1d0619e8fd8b8836880dca2a5a65
sbrij001 <sbrij001@yahoo.com>
2019-10-08 22:28:05 -0400

    created config object for fetch in submit data function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // pseudocode
 // 1.  write a method, submitData, that takes two strings arguments, one representing a user's name and the other representing a user's email.
 
+const configObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
+  body: JSON.stringify({
+    name: userName,
+    email:userEmail
+  })
+};
+
 function submitData(userName, userEmail){
-  
+  fetch('http://localhost:3000/users', configObject)
 }
