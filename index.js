a5e67f5ef1cef4084df14374e4629891f793f166
sbrij001 <sbrij001@yahoo.com>
2019-10-08 22:23:54 -0400

    created submit data function and pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Add your code here
+// pseudocode
+// 1.  write a method, submitData, that takes two strings arguments, one representing a user's name and the other representing a user's email.
+
+function submitData(userName, userEmail){
+  
+}
