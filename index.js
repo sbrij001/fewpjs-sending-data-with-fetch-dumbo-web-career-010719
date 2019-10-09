9b94a0d1826dfdb67c99cca06a51d5938ba66d76
sbrij001 <sbrij001@yahoo.com>
2019-10-08 22:39:33 -0400

    makes a POST request to /user with a name and email

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // pseudocode
 // 1.  write a method, submitData, that takes two strings arguments, one representing a user's name and the other representing a user's email.
-
-const configObject = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-  },
-  body: JSON.stringify({
-    name: userName,
-    email:userEmail
-  })
-};
-
 function submitData(userName, userEmail){
-  fetch('http://localhost:3000/users', configObject)
+  return (
+    fetch("http://localhost:3000/users", {
+      method: "Post",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+      body: JSON.stringify({
+        name: userName,
+        email: userEmail
+      })
+    })
+  )
 }
